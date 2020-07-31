f5d8611b1ef58d511d83ef0644180271aca73ecc
Dmytro Hryshyn <55950483+Dmytro-Hryshyn@users.noreply.github.com>
2020-07-31 21:06:52 +0100

    Remote control buttons Change to Enumeration

--- RomoteControl.h
+++ RomoteControl.h
@@ -1,17 +1,21 @@
-#define RemoteButt1 E318261B
-#define RemoteButt2 511DBB
-#define RemoteButt3 EE886D7F
-#define RemoteButt4 52A3D41F
-#define RemoteButt5 D7E84B1B
-#define RemoteButt6 20FE4DBB
-#define RemoteButt7 F076C13B
-#define RemoteButt8 A3C8EDDB
-#define RemoteButt9 E5CFBD7F
-#define RemoteButt0 97483BFB
-#define RemoteStar C101E57B
-#define RemoteHash F0C41643
-#define RemoteOK 488F3CBB
-#define RemoteLeft 8C22657B
-#define RemoteRight 449E79F
-#define RemoteUp 3D9AE3F7
-#define RemoteDown 1BC0157B
+
+enum  RemoteControleButton
+{
+    rButton1 = 0xE318261B,
+    rButton2 = 0x511DBB,
+    rButton3 = 0xEE886D7F,
+    rButton4 = 0x52A3D41F,
+    rButton5 = 0xD7E84B1B,
+    rButton6 = 0x20FE4DBB,
+    rButton7 = 0xF076C13B,
+    rButton8 = 0xA3C8EDDB,
+    rButton9 = 0xE5CFBD7F,
+    rButton0 = 0x97483BFB,
+    rStar = 0xC101E57B,
+    rHash = 0xF0C41643,
+    rOk = 0x488F3CBB,
+    rArrowLeft = 0x8C22657B,
+    rArrowRight = 0x449E79F,
+    rArrowUp = 0x3D9AE3F7,
+    rArrowDown = 0x1BC0157B,
+};
