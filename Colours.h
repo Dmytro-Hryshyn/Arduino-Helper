d5cee7c88e7174c57f9aa1cbdb8736d084bcd092
Dmytro Hryshyn <Prizm.drums@gmail.com>
2020-08-08 22:51:40 +0100

    Miss type fixed in Colours.h file name

---
+++ Colours.h
@@ -0,0 +1,159 @@
+#pragma region Pink Colors
+#define Pink            0xffc0cb
+#define LightPink       0xffb6c1
+#define HotPink         0xff69b4
+#define DeepPink        0xff1493
+#define PaleVioletRed   0xdb7093
+#define MediumVioletRed 0xc71585
+#pragma endregion
+
+#pragma region Purple Colors
+#define Lavender        0xe6e6fa
+#define Thistle         0xd8bfd8
+#define Plum            0xdda0dd
+#define Orchid          0xda70d6
+#define Violet          0xee82ee
+#define Magenta         0xff00ff
+#define MediumOrchid    0xba55d3
+#define DarkOrchid      0x9932cc
+#define DarkViolet      0x9400d3
+#define BlueViolet      0x8a2be2
+#define DarkMagenta     0x8b008b
+#define Purple          0x800080
+#define MediumPurple    0x9370db
+#define MediumSlateBlue 0x7b68ee
+#define SlateBlue       0x6a5acd
+#define DarkSlate       0x483d8b
+#define RebeccaPurple 	0x663399
+#define Indigo  	      0x4B0082
+#pragma endregion
+
+#pragma region Red Colors
+#define LightSalmon   	0xFFA07A	 	
+#define Salmon      	  0xFA8072	 	
+#define DarkSalmon 	    0xE9967A	 	
+#define LightCoral  	  0xF08080	 	
+#define IndianRed  	    0xCD5C5C	 	
+#define Crimson 	      0xDC143C	 	
+#define Red 	          0xFF0000	 	
+#define FireBrick 	    0xB22222	 	
+#define DarkRed 	      0x8B0000
+#pragma endregion
+
+#pragma region Orange Colors
+#define Orange 	        0xFFA500	 	
+#define DarkOrange 	    0xFF8C00	 	
+#define Coral 	        0xFF7F50	 	
+#define Tomato 	        0xFF6347	 	
+#define OrangeRed 	    0xFF4500
+#pragma region 
+
+#pragma region  Yellow Colors
+#define Gold 	           0xFFD700	 	 
+#define Yellow 	         0xFFFF00	 	 
+#define LightYellow 	   0xFFFFE0	 	 
+#define LemonChiffon 	   0xFFFACD	 	 
+#define LightGoldenRodYellow 	0xFAFAD2	 	 
+#define PapayaWhip 	    0xFFEFD5	 	 
+#define Moccasin 	      0xFFE4B5	 	 
+#define PeachPuff 	    0xFFDAB9	 	 
+#define PaleGoldenRod 	0xEEE8AA	 	 
+#define Khaki 	        0xF0E68C	 	 
+#define DarkKhaki 	    0xBDB76B
+#pragma endregion
+
+#pragma region Green Colors
+#define GreenYellow 	0xADFF2F	 	
+#define Chartreuse 	  0x7FFF00	 	
+#define LawnGreen 	  0x7CFC00	 	
+#define Lime 	0x00FF00	 	
+#define LimeGreen 	  0x32CD32	 	
+#define PaleGreen 	  0x98FB98	 	
+#define LightGreen 	  0x90EE90	 	
+#define MediumSpringGreen 	0x00FA9A	 	
+#define SpringGreen 	0x00FF7F	 	
+#define MediumSeaGreen 	0x3CB371	 	
+#define SeaGreen 	0x2E8B57	 	
+#define ForestGreen 	0x228B22	 	
+#define Green 	0x008000	 	
+#define DarkGreen 	0x006400	 	
+#define YellowGreen 	0x9ACD32	 	
+#define OliveDrab 	0x6B8E23	 	
+#define DarkOliveGreen 	0x556B2F	 	
+#define MediumAquaMarine 	0x66CDAA	 	
+#define DarkSeaGreen 	0x8FBC8F	 	
+#define LightSeaGreen 	0x20B2AA	 	
+#define DarkCyan 	0x008B8B	 	
+#define Teal 	0x008080
+#pragma endregion
+
+#pragma region Cyan Colors
+#define Aqua 	0x00FFFF	 	
+#define Cyan 	0x00FFFF	 	
+#define LightCyan 	0xE0FFFF	 	
+#define PaleTurquoise 	0xAFEEEE	 	
+#define Aquamarine 	0x7FFFD4	 	
+#define Turquoise 	0x40E0D0	 	
+#define MediumTurquoise 	0x48D1CC	 	
+#define DarkTurquoise 0x00CED1
+#pragma endregion
+
+#pragma region Blue Colors
+#define CadetBlue 	0x5F9EA0	 	
+#define SteelBlue 	0x4682B4	 	
+#define LightSteelBlue 	0xB0C4DE	 	
+#define LightBlue 	0xADD8E6	 	
+#define PowderBlue 	0xB0E0E6	 	
+#define LightSkyBlue 	0x87CEFA	 	
+#define SkyBlue 	0x87CEEB	 	
+#define CornflowerBlue 	0x6495ED	 	
+#define DeepSkyBlue 	0x00BFFF	 	
+#define DodgerBlue 	0x1E90FF	 	
+#define RoyalBlue 	0x4169E1	 	
+#define Blue 	0x0000FF	 	
+#define MediumBlue 	0x0000CD	 	
+#define DarkBlue 	0x00008B	 	
+#define Navy 	0x000080	 	
+#define MidnightBlue 	0x191970
+#pragma endregion
+
+#pragma region Brown Colors
+#define Cornsilk 	0xFFF8DC	 	
+#define BlanchedAlmond 	0xFFEBCD	 	
+#define Bisque 	0xFFE4C4	 	
+#define NavajoWhite 	0xFFDEAD	 	
+#define Wheat 	0xF5DEB3	 	
+#define BurlyWood 	0xDEB887	 	
+#define Tan 	0xD2B48C	 	
+#define RosyBrown 	0xBC8F8F	 	
+#define SandyBrown 	0xF4A460	 	
+#define GoldenRod 	0xDAA520	 	
+#define DarkGoldenRod 	0xB8860B	 	
+#define Peru 	0xCD853F	 	
+#define Chocolate 	0xD2691E	 	
+#define Olive 	0x808000	 	
+#define SaddleBrown 	0x8B4513	 	
+#define Sienna 	0xA0522D	 	
+#define Brown 	0xA52A2A	 	
+#define Maroon 	0x800000
+#pragma endregion
+
+#pragma region  White Colors
+#define White 	0xFFFFFF	 	
+#define Snow 	0xFFFAFA	 	
+#define HoneyDew 	0xF0FFF0	 	
+#define MintCream 	0xF5FFFA	 	
+#define Azure 	0xF0FFFF	 	
+#define AliceBlue 	0xF0F8FF	 	
+#define GhostWhite 	0xF8F8FF	 	
+#define WhiteSmoke 	0xF5F5F5	 	
+#define SeaShell 	0xFFF5EE	 	
+#define Beige 	0xF5F5DC	 	
+#define OldLace 	0xFDF5E6	 	
+#define FloralWhite 	0xFFFAF0	 	
+#define Ivory 	0xFFFFF0	 	
+#define AntiqueWhite 	0xFAEBD7	 	
+#define Linen 	0xFAF0E6	 	
+#define LavenderBlush 	0xFFF0F5	 	
+#define MistyRose 	0xFFE4E1
+#pragma region 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Arduino-Helper
 Self guide for Arduino projects
-- [Defined set of Colors for addressable LED strip](https://github.com/Dmytro-Hryshyn/Arduino-Helper/blob/master/Colors.h)
+- [Defined set of Colours for addressable LED strip](https://github.com/Dmytro-Hryshyn/Arduino-Helper/blob/master/Colors.h)
 - [Remote Control Encode](RomoteControl.h) for this type of [Remote Control](Images/Remote.jpg)
